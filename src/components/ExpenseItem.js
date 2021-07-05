ea85eb93f9d17cfb96229596a2ca283344db8749
nalendro16 <nalendro_agung@yahoo.com>
2021-07-06 00:03:46 +0700

    expense item list UI done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,13 @@
+import ExpnDate from './ExpnDate'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-  const month = props.date.toLocaleString('en-US', { month: 'long' })
-  const year = props.date.getFullYear()
-
   const expnTitle = props.title
   const expAmount = props.amount
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpnDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expnTitle}</h2>
         <div className="expense-item__price">{expAmount}</div>
