3afec6c1af2f62c39e11f2307a48244744bb05a1
nalendro16 <nalendro_agung@yahoo.com>
2021-07-05 21:26:55 +0700

    js date built-in function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  const expnDate = props.date
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const year = props.date.getFullYear()
+
   const expnTitle = props.title
   const expAmount = props.amount
 
   return (
     <div className="expense-item">
       <div>
-        <div>{props.date.toLocaleString('en-US', { month: 'long' })}</div>
-        <div>Year</div>
-        <div>Date</div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
       <div className="expense-item__description">
         <h2>{expnTitle}</h2>
