62e42a50ab8f407487bf009887a88ce40a4f7e4c
nalendro16 <nalendro_agung@yahoo.com>
2021-07-19 13:40:13 +0700

    form handling with multiple state

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,67 @@
+import './ExpenseForm.css'
+import { useState } from 'react'
+
+const ExpenseForm = () => {
+  const [entTitle, setEntTitle] = useState('')
+  const [entAmount, setEntAmount] = useState('')
+  const [entDate, setEntDate] = useState('')
+
+  const titleChange = (e) => {
+    setEntTitle(e.target.value)
+  }
+  const amountChange = (e) => {
+    setEntAmount(e.target.value)
+  }
+  const dateChange = (e) => {
+    setEntDate(e.target.value)
+  }
+
+  const submitHandle = (e) => {
+    e.preventDefault()
+    const expenseData = {
+      title: entTitle,
+      amount: entAmount,
+      date: new Date(entDate),
+    }
+    console.log(expenseData)
+    setEntTitle('')
+    setEntAmount('')
+    setEntDate('')
+  }
+
+  return (
+    <form onSubmit={submitHandle}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input type="text" value={entTitle} onChange={titleChange} />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={entAmount}
+            onChange={amountChange}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={entDate}
+            onChange={dateChange}
+          />
+        </div>
+        <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default ExpenseForm
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import './NewExpense.css'
+import Form from './ExpenseForm'
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <Form />
+    </div>
+  )
+}
+
+export default NewExpense
