7fb1d231dfdb92dcdd2ffb19460ad73029bfbd57
nalendro16 <nalendro_agung@yahoo.com>
2021-07-20 21:58:44 +0700

    remove button for usestate dumy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,17 @@
-import { useState } from 'react'
-
 import ExpnDate from './ExpnDate'
 import './ExpenseItem.css'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
-  // const expnTitle = props.title
   const expAmount = props.amount
-
-  const [title, setTitle] = useState(props.title)
-
-  const clickHandler = () => {
-    setTitle('Update!')
-    console.log(title)
-  }
 
   return (
     <Card className="expense-item">
       <ExpnDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{expAmount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
